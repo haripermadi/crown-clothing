629ccee13a243d9a2f9fd7e3a623fd717e4b7ef9
haripermadi <harinurpermadi@gmail.com>
2020-06-15 18:24:35 +0700

    add styled component for some

--- src/components/custom-button/custom-button.component.js
+++ src/components/custom-button/custom-button.component.js
@@ -1,17 +1,23 @@
 import React from "react";
 import "./custom-button.styles.scss";
 
-const CustomButton = ({ children, isGoogleSignIn, inverted, ...others }) => {
-  return (
-    <button
-      className={`${inverted ? "inverted" : ""} ${
-        isGoogleSignIn ? "google-sign-in" : ""
-      } custom-button`}
-      {...others}
-    >
-      {children}
-    </button>
-  );
+import { CustomButtonContainer } from "./custom-button.styles";
+
+// const CustomButton = ({ children, isGoogleSignIn, inverted, ...others }) => {
+//   return (
+//     <button
+//       className={`${inverted ? "inverted" : ""} ${
+//         isGoogleSignIn ? "google-sign-in" : ""
+//       } custom-button`}
+//       {...others}
+//     >
+//       {children}
+//     </button>
+//   );
+// };
+
+const CustomButton = ({ children, ...others }) => {
+  return <CustomButtonContainer {...others}>{children}</CustomButtonContainer>;
 };
 
 export default CustomButton;
